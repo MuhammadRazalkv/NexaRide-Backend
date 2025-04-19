7b0d61a49a7ffa9c21faf74291414965277f0e80
MuhammadRazalkv <rasalkv21@gmail.com>
2025-04-19 14:17:50 +0530

    fix: Cloudinary key to env

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,8 +1,8 @@
 import { v2 } from "cloudinary";
 
 v2.config({
-    api_key:"975787696957148",
-    cloud_name:"di2fg3dfh",
+    api_key:process.env.CLOUDINARY_API_KEY,
+    cloud_name:process.env.CLOUDINARY_NAME,
     api_secret:process.env.CLOUDINARY_SECRET
 })
 
